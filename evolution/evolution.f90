747052a23c11fbbc6d0e30fa5aca330aaf476fb8
michael <mpalmer16@gmail.com>
2015-07-30 23:08:37 -0500

    eating, WERE EATING!

--- evolution/evolution.f90
+++ evolution/evolution.f90
@@ -1,45 +1,47 @@
 program evolution
-    
+
     use functions
     use types
-    
+
     implicit none
-    
+
     integer, parameter :: WIDTH = 100, HEIGHT = 30, PLT_NRG = 80, REPR_NRG = 100
     integer, parameter :: MAX_ARR_SIZE = WIDTH * HEIGHT
-    
+
     integer :: i
-    
+
     type(Animal), dimension(0:MAX_ARR_SIZE) :: Animals
     type(Plant), dimension(0:MAX_ARR_SIZE)  :: Plants
-    
+
     type(Animal) :: a, b
     type(Plant)  :: p
-    
+
     a%alive = .true.
     a%x     = 10
     a%y     = 10
-    
+
     b = a%copy()
-    
+
     b%x = 1
     b%y = 2
-    
+
+    call a%eat(PLT_NRG)
+
     p%alive = .true.
     p%x     = 12
     p%y     = 13
-    
+
     Animals(0) = a
     Animals(1) = b
     Plants(0)  = p
-    
+
     write(*,*) "ANIMALS:"
     do i = 0, MAX_ARR_SIZE
         if (Animals(i)%alive) then
             call Animals(i)%show
         end if
     end do
-    
+
     write(*,*) "PLANTS:"
     do i = 0, MAX_ARR_SIZE
         if (Plants(i)%alive) then
--- evolution/types.f90
+++ evolution/types.f90
@@ -3,45 +3,46 @@ module types
     implicit none
     private
 
-    
+
     type, public :: Animal
-    
+
         logical                 :: alive = .false.
         integer                 :: x, y, energy = 1000, direction = 0
         integer, dimension(0:8) :: genes
-        
+
     contains
-    
+
         procedure :: show   => describe_animal
         procedure :: copy   => get_new_animal
         procedure :: cpy_gn => copy_genes
-        
+        procedure :: eat    => eat_animal
+
     end type Animal
-    
-    
+
+
     type, public :: Plant
-        
+
         logical :: alive = .false.
         integer :: x, y
-    
+
     contains
-    
+
         procedure :: show => describe_plant
-    
+
     end type Plant
-    
-    
+
+
 contains
-    
+
     subroutine describe_animal(this)
-        
+
         100 FORMAT(a,i3,a,i3,a)
         200 FORMAT(a,i4)
         300 FORMAT(a,i1)
         400 FORMAT(a)
-        
+
         class(Animal) :: this
-        
+
         if (this%alive) then
             write(*,100) "Location: (", this%x, ",", this%y, " )"
             write(*,200) "Energy:  ", this%energy
@@ -49,50 +50,59 @@ contains
         else
             write(*,400) "This animal is dead!"
         end if
-        
+
     end subroutine describe_animal
-    
-    
+
+
     subroutine describe_plant(this)
-        
+
         100 FORMAT(a,i3,a,i3,a)
         400 FORMAT(a)
-        
+
         class(Plant) :: this
-        
+
         if (this%alive) then
             write(*,100) "Location: (", this%x, ",", this%y, " )"
         else
             write(*,400) "This plant is dead!"
         end if
-        
+
     end subroutine describe_plant
-    
-    
+
+
     pure function get_new_animal(this) result(new_a)
-    
+
         class(Animal), intent(in) :: this
         type(Animal) :: new_a
-        
+
         new_a%alive     = .true.
         new_a%x         = this%x
         new_a%y         = this%y
         new_a%energy    = this%energy
         new_a%genes     = this%cpy_gn()
-        
+
     end function get_new_animal
-    
-    
+
+
     pure function copy_genes(this) result(c_genes)
-        
+
         class(Animal), intent(in)   :: this
         integer, dimension(0:8)     :: c_genes
         integer                     :: i
-        
+
         do i = 0, 8
             c_genes(i) = this%genes(i)
         end do
-        
+
     end function copy_genes
+
+    subroutine eat_animal(this, nrg)
+
+        class(Animal) :: this
+        integer :: nrg
+
+        this%energy = this%energy + nrg
+
+    end subroutine eat_animal
 
 end module types
