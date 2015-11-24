6bde401c39843ebe7992ace9c10cc79f7a8cb655
michael <mpalmer16@gmail.com>
2015-11-23 21:22:22 -0600

    new work

--- evolution/functions.f90
+++ evolution/functions.f90
@@ -2,66 +2,66 @@ module functions
 
     use constants
     use types
-    
+
     implicit none
-    
+
 contains
-    
+
     subroutine push_plant(plant_array, p)
-        
+
         type(Plant) :: p
         integer     :: i
         type(Plant), dimension(0:MAX_ARR_SIZE) :: plant_array
-        
+
         do i = 0, MAX_ARR_SIZE
             if (.not. plant_array(i)%alive) then
                 plant_array(i) = p
                 exit
             end if
         end do
-        
+
     end subroutine push_plant
-    
-    
+
+
     subroutine push_animal(animal_array, a)
-    
+
         type(Animal) :: a
         integer      :: i
         type(Animal), dimension(0:MAX_ARR_SIZE) :: animal_array
-        
+
         do i = 0, MAX_ARR_SIZE
             if (.not. animal_array(i)%alive) then
                 animal_array(i) = a
                 exit
             end if
         end do
-        
+
     end subroutine push_animal
-    
-    
+
+
     function make_plant() result(p)
-        
+
         type(Plant) :: p
-        
+
         p%alive = .true.
         p%x = random(WIDTH)
         p%y = random(HEIGHT)
-        
+
     end function make_plant
-        
-    
+
+
     subroutine simulate_day(as, ps)
-        
+
         type(Animal), dimension(0:MAX_ARR_SIZE) :: as
         type(Plant),  dimension(0:MAX_ARR_SIZE) :: ps
         type(Animal), dimension(0:MAX_ARR_SIZE) :: hold_as
         integer :: i, add_count = 0
-        
+
         call push_plant(ps, make_plant())
         call push_plant(ps, make_plant())
-        
+
         do i = 0, MAX_ARR_SIZE
-            if (as(i)%alive) then 
+            if (as(i)%alive) then
                 call as(i)%move()
                 call as(i)%turn()
                 call as(i)%eat(ps)
@@ -71,71 +71,71 @@ contains
                 end if
             end if
         end do
-        
+
         do i = 0, add_count - 1
             call push_animal(as, hold_as(i))
         end do
-        
+
         call remove_dead(as)
-        
+
     end subroutine simulate_day
-    
+
     recursive subroutine evolve(as, ps)
-        
+
         type(Animal), dimension(0:MAX_ARR_SIZE) :: as
         type(Plant),  dimension(0:MAX_ARR_SIZE) :: ps
         integer :: i, days
-        
+
         read *, days
-        
+
         if (days .eq. 999) then
             return
         else
             do i = 1, days
-            
+
                 call simulate_day(as, ps)
-            
+
             end do
             call draw_world(as, ps)
         end if
-        
+
         call evolve(as, ps)
-        
+
     end subroutine evolve
-    
+
     subroutine remove_dead(arr)
-    
+
         type(Animal), dimension(0:MAX_ARR_SIZE) :: arr
         integer :: i
-        
+
         do i = 0, MAX_ARR_SIZE
-        
+
             if (arr(i)%alive .and. arr(i)%energy < 1) then
                 arr(i)%alive = .false.
             end if
-            
+
         end do
-        
+
     end subroutine remove_dead
-    
+
 
     function random(r) result(n)
-    
+
         integer :: r, n
         real :: rnd
-        
+
         call random_number(rnd)
-        
+
         n = int(rnd * r)
-    
+
     end function random
-    
+
 
     subroutine init_random_seed()
 
         integer :: t, sz, clock
         integer, dimension(:), allocatable :: seed
-    
+
         call random_seed(size = sz)
         allocate(seed(sz))
         call system_clock(count = clock)
@@ -144,14 +144,14 @@ contains
         deallocate(seed)
 
     end subroutine init_random_seed
-    
+
     subroutine draw_world(as, ps)
 
         type(Animal), dimension(0:MAX_ARR_SIZE) :: as
         type(Plant),  dimension(0:MAX_ARR_SIZE) :: ps
         integer :: x, y, c
         logical :: is_a = .false., is_p = .false.
-        
+        call execute_command_line('clear')
         do y = 0, HEIGHT
             write(*,'(a)', advance="no") "|"
             do x = 0, WIDTH
@@ -179,41 +179,41 @@ contains
             end do
             write(*,*) "|"
         end do
-    
+
     end subroutine draw_world
 
 
     pure function count_animals(a_arr) result(n)
-        
+
         type(Animal), intent(in), dimension(0:MAX_ARR_SIZE) :: a_arr
         integer :: i, n
-        
+
         do i = 0, MAX_ARR_SIZE
             if (a_arr(i)%alive) then
                 n = n + 1
             end if
         end do
     end function count_animals
-    
-    
+
+
     pure function count_plants(p_arr) result(n)
-        
+
         type(Plant), intent(in), dimension(0:MAX_ARR_SIZE) :: p_arr
         integer :: i, n
-        
+
         do i = 0, MAX_ARR_SIZE
             if (p_arr(i)%alive) then
                 n = n + 1
             end if
         end do
     end function count_plants
-    
-    
+
+
     subroutine show_animals(as)
-    
+
         type(Animal), dimension(0:MAX_ARR_SIZE) :: as
         integer :: i
-        
+
         do i = 0, MAX_ARR_SIZE
             if (as(i)%alive) then
                 call as(i)%show()
